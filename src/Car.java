2588220d1cd2d7e96f858d6197f5b1c140bfb6eb
Hitthenutz <matthewquan001@gmail.com>
2024-04-07 23:09:15 -0700

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -45,6 +45,14 @@ public class Car {
         this.year = year;
         this.customer.setSsn(customer.getSsn());
     }
+    public Car(String plate, String make, String model, String vin, int year) {
+        this.plate = plate;
+        this.make = make;
+        this.model = model;
+        this.vin = vin;
+        this.year = year;
+    }
+
     public Car(){
 
     }
